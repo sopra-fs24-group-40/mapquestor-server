ed8c67d88ccaa3a9dba7671b51536377b950a708
bhalf <benjamin.halfar@uzh.ch>
2024-04-16 14:32:40 +0200

    Added cities file and changed service

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/City.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/City.java
@@ -1,14 +1,11 @@
 package ch.uzh.ifi.hase.soprafs24.entity;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Entity
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class City {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     private String name;
     private String capital;
@@ -26,44 +23,5 @@ public class City {
         this.latitude = latitude;
     }
 
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCapital() {
-        return capital;
-    }
-
-    public void setCapital(String capital) {
-        this.capital = capital;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
 }
