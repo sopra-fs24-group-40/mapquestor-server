db5ed85ad6dbf6f48a0c911364e800edb67702a6
LaughingF0x <115981865+LaughingF0x@users.noreply.github.com>
2024-05-19 15:43:32 +0200

    MessageHandler and Message Tests #86

--- src/test/java/ch/uzh/ifi/hase/soprafs24/messages/MessageTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/messages/MessageTest.java
@@ -17,9 +17,9 @@ public class MessageTest {
     }
 
     @Test
-    @DisplayName("Message fields are set correctly by constructor")
+    @DisplayName("Message constructor sets fields correctly")
     public void messageConstructorSetsFieldsCorrectly() {
-        message = new Message<>("sender", "content", MessageType.CHAT);
+        Message<String> message = new Message<>("sender", "content", MessageType.CHAT);
 
         Assertions.assertEquals("sender", message.getFrom());
         Assertions.assertEquals("content", message.getContent());
@@ -27,8 +27,9 @@ public class MessageTest {
     }
 
     @Test
-    @DisplayName("Message fields are updated correctly by setters")
+    @DisplayName("Message setters update fields correctly")
     public void messageSettersUpdateFieldsCorrectly() {
+        Message<String> message = new Message<>();
         message.setFrom("sender");
         message.setContent("content");
         message.setType(MessageType.CHAT);
@@ -41,10 +42,40 @@ public class MessageTest {
     @Test
     @DisplayName("Message constructor handles null content")
     public void messageConstructorHandlesNullContent() {
-        message = new Message<>("sender", null, MessageType.CHAT);
+        Message<String> message = new Message<>("sender", null, MessageType.CHAT);
 
         Assertions.assertEquals("sender", message.getFrom());
         Assertions.assertNull(message.getContent());
         Assertions.assertEquals(MessageType.CHAT, message.getType());
+    }
+
+    @Test
+    @DisplayName("Message constructor handles null sender")
+    public void messageConstructorHandlesNullSender() {
+        Message<String> message = new Message<>(null, "content", MessageType.CHAT);
+
+        Assertions.assertNull(message.getFrom());
+        Assertions.assertEquals("content", message.getContent());
+        Assertions.assertEquals(MessageType.CHAT, message.getType());
+    }
+
+    @Test
+    @DisplayName("Message constructor handles null type")
+    public void messageConstructorHandlesNullType() {
+        Message<String> message = new Message<>("sender", "content", null);
+
+        Assertions.assertEquals("sender", message.getFrom());
+        Assertions.assertEquals("content", message.getContent());
+        Assertions.assertNull(message.getType());
+    }
+
+    @Test
+    @DisplayName("Message constructor handles all null fields")
+    public void messageConstructorHandlesAllNullFields() {
+        Message<String> message = new Message<>(null, null, null);
+
+        Assertions.assertNull(message.getFrom());
+        Assertions.assertNull(message.getContent());
+        Assertions.assertNull(message.getType());
     }
 }
